addb1c932e017b7f5e4360adb3e3c79389f2c03e
SamWiseTarly <biasamc@gmail.com>
2015-07-29 11:52:09 -0400

    Create db_config.php

---
+++ db_config.php
@@ -0,0 +1,13 @@
+DB_CONFIG.PHP
+
+<?php
+	/*
+	*database connection variables
+	*/
+
+	define('DB_USER', "root");//user
+	define('DB_PASSWORD', "");// user password
+	define('DB_DATABASE', "androidtest");//database name
+	define('DB_SERVER', "localhost");// db server
+
+?>
